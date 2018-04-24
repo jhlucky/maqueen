6c44d321d0153c6c40087545632fa51460cef68c
ouki-wang <358023925@qq.com>
2018-04-24 16:05:38 +0800

    4.24-2

--- ir.ts
+++ ir.ts
@@ -1,19 +1,11 @@
 
 namespace Ir{
-    /**
-     * button pushed.
-     */
-    //% blockId=ir_received_left_event
-    //% block="on |%btn| button pressed" shim=IR::onPressEvent
+    //% advanced=true shim=IR::onPressEvent
     function onPressEvent(btn: RemoteButton, body: () => void): void { 
         return
     }
 
-    /**
-     * initialises local variablesssss
-     */
-    //% blockId=ir_init
-    //% block="connect ir receiver to %pin" shim=IR::init
+    //% advanced=true shim=IR::init
     function init(pin: Pins): void { 
         return
     }
