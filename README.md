a552e46e0b81b4aa6543b1100b901de6eda5e8dd
ouki-wang <358023925@qq.com>
2018-04-24 15:27:03 +0800

    4.24-0

---
+++ README.md
@@ -0,0 +1,12 @@
+for PXT/microbit
+
+how do i ir??????
+
+for using an ir receiver PXT on the microbit
+
+
+known issues:
+
+ - it is a little slow sometimes
+ 
+ - press and hold does not work (yet)
---
+++ ir.ts
@@ -0,0 +1,22 @@
+
+namespace Ir}{
+    /**
+     * button pushed.
+     */
+    //% blockId=ir_received_left_event
+    //% block="on |%btn| button pressed" shim=IR::onPressEvent
+    function onPressEvent(btn: RemoteButton, body: () => void): void { 
+        return
+    }
+
+    /**
+     * initialises local variablesssss
+     */
+    //% blockId=ir_init
+    //% block="connect ir receiver to %pin" shim=IR::init
+    function init(pin: Pins): void { 
+        return
+    }
+
+
+}
