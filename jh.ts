36febe1d86cb66f7d65b7b09940f64aff8f0480d
ouki-wang <358023925@qq.com>
2018-04-28 15:32:34 +0800

    4.28-13

--- jh.ts
+++ jh.ts
@@ -5,20 +5,17 @@
 namespace DFIR{
 
   
-  //% blockId=ir_received_left_event
-  //% block="on |%btn| button pressed" shim=IR::onPressEvent
-  export function onPressEvent(btn: RemoteButton, body: () => void):void{
-    return
-  }
-  
+
   //% blockId=ir_init
   //% block="connect ir receiver to %pin" shim=IR::init
-  export function init(pin: Pins): void{
-    return
+  export function init2(pin: Pins): void{
+    init(pin)
   }
     
-  
-  
+  //% advanced=true shim=IR::init
+  function init(pin: Pins):void{
+    return
+  }
   
   
 }
