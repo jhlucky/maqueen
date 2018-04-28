4d5fda691edd8177a5e265e8f9aed99eda7fa671
ouki-wang <358023925@qq.com>
2018-04-28 11:05:47 +0800

    4.28-9

--- shims.d.ts
+++ shims.d.ts
@@ -1,5 +1,6 @@
 // Auto-generated. Do not edit.
 
+let cb: Action
 
     //% color=50 weight=80
     //% icon="\uf1eb"
@@ -36,7 +37,18 @@ declare namespace IR {
     //% mutateText=Packeta
     //% mutateDefaults="myparam:message"
     //% blockId=obloq_mqttCallbackUser block="on obloq received"
-    function obloq_mqttCallbackUser(cb: (packet: Packeta) => void): void; 
+    function obloq_mqttCallbackUser(cb: (packet: Packeta) => void) {
+        obloq_mqttCallback(() => {
+            const packet = new Packeta();
+            packet.mye = e
+            packet.myparam = param;
+            cb(packet)
+        });
+    }
+    
+    void obloq_mqttCallback(a: Action): void{
+        cb = a
+    }
     
     
 }
