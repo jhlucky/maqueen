48663f92d11bf75b6a3203ec4bf33a1e75b25f1a
ouki-wang <358023925@qq.com>
2018-04-28 14:36:31 +0800

    4.28-13

---
+++ jh.ts
@@ -0,0 +1,24 @@
+
+
+
+//% weight=10 color=#008B00 icon="\uf1eb" block="DFIR"
+namespace DFIR{
+
+  
+  //% blockId=ir_received_left_event
+  //% block="on |%btn| button pressed" shim=IR::onPressEvent
+  export function onPressEvent(btn: RemoteButton, body: () => void){
+    return
+  }
+  
+  //% blockId=ir_init
+  //% block="connect ir receiver to %pin" shim=IR::init
+  export function init(pin: Pins): void{
+    return
+  }
+    
+  
+  
+  
+  
+}
