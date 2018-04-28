0a1360f67cb3f53014affa2609f29e2f683e87ed
ouki-wang <358023925@qq.com>
2018-04-28 18:04:57 +0800

    4.28-13

--- jh.ts
+++ jh.ts
@@ -1,24 +1,4 @@
-enum Pins {
-    P0 = 3,
-    P1 = 2,
-    P2 = 1,
-    P3 = 4,
-    P4 = 5,
-    P5 = 17,
-    P6 = 12,
-    P7 = 11,
-    P8 = 18,
-    P9 = 10,
-    P10 = 6,
-    P11 = 26,
-    P12 = 20,
-    P13 = 23,
-    P14 = 22,
-    P15 = 21,
-    P16 = 16,
-    P19 = 0,
-    P20 = 30,
-}
+
 
 
 //% weight=10 color=#008B00 icon="\uf1eb" block="DFIR"
@@ -32,7 +12,7 @@ namespace IR{
     //% blockId=ir_init2
     //% block="connect ir receiver to pin %pin"
     export function init2(pin: Pins): void{
-        init55(pin)
+        init55(11)
         
     }
       
