3147c4578c80ee490d0991ffbc4a15b82aa52cd8
ouki-wang <358023925@qq.com>
2018-04-28 15:51:49 +0800

    4.28-13

--- jh.ts
+++ jh.ts
@@ -6,16 +6,16 @@ namespace DFIR{
 
   
 
-  //% blockId=ir_init
-  //% block="connect ir receiver to %pin"
-  export function init2(pin: Pins): void{
-    return init(pin)
-  }
-    
-  //% advanced=true shim=IR::init
-  function init(pin: Pins):void{
-    return
-  }
+    //% blockId=ir_init
+    //% block="connect ir receiver to %pin"
+    export function init2(pin: Pins): void{
+        init(pin)
+    }
+      
+    //% advanced=true shim=IR::init
+    function init(pin: Pins):void{
+        return
+    }
   
   
 }
