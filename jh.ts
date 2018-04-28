54cb38c093430dcc8d3892a174c98c8e46d62b98
ouki-wang <358023925@qq.com>
2018-04-28 16:58:44 +0800

    4.28-13

--- jh.ts
+++ jh.ts
@@ -4,7 +4,7 @@
 //% weight=10 color=#008B00 icon="\uf1eb" block="DFIR"
 namespace DFIR{
 
-    //% advanced=true shim=IR::init
+    //% advanced=true shim=IR::init55
     function init55(pin: Pins):void{
         return
     }
@@ -12,7 +12,8 @@ namespace DFIR{
     //% blockId=ir_init2
     //% block="connect ir receiver to %pin"
     export function init2(pin: Pins): void{
-        init55(pin)
+        //init55(pin)
+        ;
     }
       
     
