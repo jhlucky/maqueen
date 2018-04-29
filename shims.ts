bc36d11c741f347f33a4a81df7c1ed64d4d3ed62
ouki-wang <358023925@qq.com>
2018-04-29 12:14:53 +0800

    29.5

--- shims.ts
+++ shims.ts
@@ -2,23 +2,22 @@
 
 
 
-    //% color=50 weight=80
-    //% icon="\uf1eb"
-declare namespace IR {
+//% weight=10 color=#008B00 icon="\uf1eb" block="DFIR"
+namespace DFIR {
 
     /**
      * button pushed.
      */
     //% blockId=ir_received_left_event
     //% block="on |%btn| button pressed" shim=IR::onPressEvent
-    function onPressEvent(btn: RemoteButton, body: () => void): void;
+    export function onPressEvent(btn: RemoteButton, body: () => void): void;
 
     /**
      * initialises local variablesssss
      */
     //% blockId=ir_init
     //% block="connect ir receiver to %pin" shim=IR::init
-    function init(pin: Pins): void;
+    export function init(pin: Pins): void;
 }
 
 // Auto-generated. Do not edit. Really.
