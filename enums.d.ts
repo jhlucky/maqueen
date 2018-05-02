cfbc4ba681537997cbcf25efeb94df0bcb944299
ouki-wang <358023925@qq.com>
2018-05-02 14:53:37 +0800

    5.2-5

--- enums.d.ts
+++ enums.d.ts
@@ -25,15 +25,27 @@
 
 
     declare enum RemoteButton {
-    Power = 0x0,
-    Up = 0x5,
-    Left = 0x8,
-    Right = 0xA,
-    Down = 0xD,
-    OK = 0x9,
-    Plus = 0x4,
-    Minus = 0xC,
-    Back = 0x6,
+    Power = 0x00,
+    VolUp = 0x01,
+    FuncStop = 0x02,
+    LeftTwo = 0x04,
+    Suspended = 0x05,
+    RightTwo = 0x06,
+    Down = 0x08,
+    VolDown = 0x09,
+    Up = 0x0a,
+    Zero = 0x0c,
+    EQ = 0x0d,
+    StRept = 0x0e,
+    One = 0x10,
+    Two = 0x11,
+    Three = 0x12,
+    Four = 0x14,
+    Five = 0x15,
+    Six = 0x16,
+    Seven = 0x18,
+    Eight = 0x19,
+    Nine = 0x1a,
     }
     
 declare namespace IR {
