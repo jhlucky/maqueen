668f4b3b6f4372b00c4592c488abc634f1d7d781
ouki-wang <358023925@qq.com>
2018-05-08 14:53:49 +0800

    5.8-2

--- shims.ts
+++ shims.ts
@@ -98,6 +98,15 @@ namespace IR{
             default: return d ;
         }
     }
+    
+    //% weight=90
+    //% blockId=motor_MotorRun block="Motor|%index|dir|%Dir|speed|%speed"
+    //% speed.min=0 speed.max=256
+    //% index.fieldEditor="gridpicker" index.fieldOptions.columns=2
+    //% direction.fieldEditor="gridpicker" direction.fieldOptions.columns=2
+    export function MotorRun(index: Motors, direction:Dir, speed: number): void {
+        
+    }
 
   
 }
