9ff63ec17f567091d13573356536017a47a194b6
ouki-wang <358023925@qq.com>
2018-05-11 14:01:50 +0800

    5.11

--- shims.ts
+++ shims.ts
@@ -118,7 +118,7 @@ namespace IR{
     
     //% weight=90
     //% blockId=motor_MotorRun block="Motor|%index|dir|%Dir|speed|%speed"
-    //% speed.min=0 speed.max=256
+    //% speed.min=0 speed.max=255
     //% index.fieldEditor="gridpicker" index.fieldOptions.columns=2
     //% direction.fieldEditor="gridpicker" direction.fieldOptions.columns=2
     export function MotorRun(index: Motors, direction:Dir, speed: number): void {
