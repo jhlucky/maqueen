e672d2d6e72b3008dfd0a4918e34cd4ec0246675
ouki-wang <358023925@qq.com>
2018-05-14 14:18:33 +0800

    5.14-4

--- ir.cpp
+++ ir.cpp
@@ -157,7 +157,7 @@ namespace IR {
     now = tsb.read_ms();
     if(now - lastact[(RemoteButton)buf[2]] < 100) return;
     lastact[(RemoteButton)buf[2]] = now;
-    msg=<number>buf[2];
+    msg=(RemoteButton)buf[2];
     cA(actions[(RemoteButton)buf[2]]);      
   }
 
